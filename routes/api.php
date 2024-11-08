58da77b3e2a733462b17750232b49bc9b608d109
Christopher Lemmer <lemmer.j.christopher@gmail.com>
2024-11-08 17:55:19 -0600

    Update route to match requirements and update path to new controller location

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\Api\V1\BestSellersController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-    Route::get('/best-sellers', [BestSellersController::class, 'history']);
-});
+Route::post('1/nyt/best-sellers', App\Http\Controllers\Api\V1\BestSellersController::class)
+    ->name('api.v1.best-sellers');
